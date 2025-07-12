1366cef60dcb6996478806c443ec96fbb64306c8
Adam Rocha <adamrocha13@gmail.com>
2025-07-11 17:08:46 -0700

    Add setup-kubeconfig script for EKS cluster configuration

---
+++ scripts/setup-kubeconfig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script sets up the kubeconfig for an EKS cluster.
+
+
+# CLUSTER_NAME="eks-demo-cluster"
+# REGION="us-east-1"
+
+# aws eks update-kubeconfig \
+#     --region "$REGION" \
+#     --name "$CLUSTER_NAME"
