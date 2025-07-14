7393fbe321430fd9854febb3ab8e937b4984c8ad
Adam Rocha <adamrocha13@gmail.com>
2025-07-14 14:21:56 -0700

    Add Kubernetes provider configuration for EKS cluster

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,3 +4,9 @@ provider "aws" {
   profile                  = "prom_infradmin"
   region                   = "us-east-1"
 }
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
