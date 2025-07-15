c9e7c7d63e5b086fea708a6898af7fd6a00334db
Adam Rocha <adamrocha13@gmail.com>
2025-07-14 18:50:28 -0700

    Add output for hello-world service endpoint in EKS configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,3 +14,8 @@ output "cluster_ca" {
 output "node_role_arn" {
   value = aws_iam_role.eks_nodes.arn
 }
+
+output "hello_world_service_endpoint" {
+  description = "External endpoint of the hello-world LoadBalancer service"
+  value       = "http://${kubernetes_service.hello_world.status[0].load_balancer[0].ingress[0].hostname}"
+}
