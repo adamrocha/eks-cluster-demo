a105c2ce780110571e6bd11862d3cc9cabfbf350
Adam Rocha <adamrocha13@gmail.com>
2025-07-15 20:15:26 -0700

    Add KMS key and alias for S3 bucket encryption

--- terraform/keys.tf
+++ terraform/keys.tf
@@ -20,6 +20,33 @@ resource "aws_kms_key" "eks_secrets" {
   })
 }
 
+resource "aws_kms_key" "s3_bucket_key" {
+  description             = "KMS key for S3 bucket encryption"
+  deletion_window_in_days = 10 # Required for KMS key deletion
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "key-default-1",
+    Statement = [
+      {
+        Sid    = "Allow root account to manage the key",
+        Effect = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_kms_alias" "s3_bucket_key_alias" {
+  name          = "alias/s3-bucket-key" # Choose a descriptive alias
+  target_key_id = aws_kms_key.s3_bucket_key.id
+}
+
 resource "aws_kms_key" "cloudwatch_logs" {
   description         = "KMS key for CloudWatch Logs"
   enable_key_rotation = true
