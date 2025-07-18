61792ee768d1298a92da81a887c4e62d0751370e
Adam Rocha <adamrocha13@gmail.com>
2025-07-18 12:46:55 -0700

    Comment out Terraform backend configuration for S3 state management

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-2727"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-state-bucket-2727"
+#     key            = "envs/dev/terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#   }
+# }
