2353cda74434ea824437315e87726a0071a57b64
Adam Rocha <adamrocha13@gmail.com>
2025-07-25 14:43:57 -0700

    Comment out unused output variables in outputs.tf for clarity

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,24 +3,28 @@ output "cluster_endpoint" {
   value       = aws_eks_cluster.eks.endpoint
 }
 
-output "cluster_name" {
-  description = "Name of the EKS cluster"
-  value       = aws_eks_cluster.eks.name
-}
+# output "cluster_name" {
+#   description = "Name of the EKS cluster"
+#   value       = aws_eks_cluster.eks.name
+# }
 
-output "cluster_ca" {
-  description = "Base64 encoded CA certificate for the EKS cluster"
-  value       = aws_eks_cluster.eks.certificate_authority[0].data
-  sensitive   = true
-}
+# output "cluster_ca" {
+#   description = "Base64 encoded CA certificate for the EKS cluster"
+#   value       = aws_eks_cluster.eks.certificate_authority[0].data
+#   sensitive   = true
+# }
 
-output "node_role_arn" {
-  description = "ARN of the EKS node group IAM role"
-  value       = aws_iam_role.eks_nodes.arn
-}
+# output "node_role_arn" {
+#   description = "ARN of the EKS node group IAM role"
+#   value       = aws_iam_role.eks_nodes.arn
+# }
 
 output "hello_world_service_endpoint" {
   description = "External endpoint of the hello-world LoadBalancer service"
   value       = "http://${kubernetes_service.hello_world_service.status[0].load_balancer[0].ingress[0].hostname}"
 }
 
+output "ecrimage" {
+  description = "Docker image to be used in the deployment"
+  value       = kubernetes_deployment.hello_world.spec[0].template[0].spec[0].container[0].image
+}
