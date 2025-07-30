98145b469d0d195f0d52129c96c6628aa4a2af1b
Adam Rocha <adamrocha13@gmail.com>
2025-07-30 16:51:01 -0700

    Comment out unused helm_release resources for clarity and organization

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,40 +1,40 @@
-resource "helm_release" "cloudwatch_exporter" {
-    depends_on       = [aws_eks_cluster.eks]
-    name             = "prometheus-cloudwatch-exporter"
-    repository       = "https://prometheus-community.github.io/helm-charts"
-    chart            = "prometheus-cloudwatch-exporter"
-    namespace        = "monitoring"
-    create_namespace = true
-    timeout          = 600
+# resource "helm_release" "cloudwatch_exporter" {
+#   depends_on       = [aws_eks_node_group.node_group]
+#   name             = "prometheus-cloudwatch-exporter"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   chart            = "prometheus-cloudwatch-exporter"
+#   namespace        = "monitoring"
+#   create_namespace = true
+#   timeout          = 600
 
-    set {
-        name  = "service.type"
-        value = "ClusterIP"
-    }
+#   set {
+#     name  = "service.type"
+#     value = "ClusterIP"
+#   }
 
-    set {
-        name  = "aws.region"
-        value = var.region
-    }
-}
+#   set {
+#     name  = "aws.region"
+#     value = var.region
+#   }
+# }
 
-resource "helm_release" "kube_state_metrics" {
-  depends_on       = [aws_eks_cluster.eks]
-  name             = "kube-state-metrics"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-state-metrics"
-  namespace        = "monitoring"
-  create_namespace = true
-  timeout          = 600  
+# resource "helm_release" "kube_state_metrics" {
+#   depends_on       = [aws_eks_node_group.node_group]
+#   name             = "kube-state-metrics"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   chart            = "kube-state-metrics"
+#   namespace        = "monitoring"
+#   create_namespace = true
+#   timeout          = 600
 
-  set {
-    name  = "service.type"
-    value = "LoadBalancer"
-  }
-}
+#   set {
+#     name  = "service.type"
+#     value = "LoadBalancer"
+#   }
+# }
 
 resource "helm_release" "prometheus" {
-  depends_on       = [aws_eks_cluster.eks]
+  depends_on       = [aws_eks_node_group.node_group]
   name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
