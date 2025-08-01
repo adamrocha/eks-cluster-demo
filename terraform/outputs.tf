1b522680f4a7d81ba0c882884a54909ffd631a29
Adam Rocha <adamrocha13@gmail.com>
2025-08-01 12:19:15 -0700

    Comment out hello world service endpoint output for clarity

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,10 +19,10 @@
 #   value       = aws_iam_role.eks_nodes.arn
 # }
 
-output "hello_world_service_endpoint" {
-  description = "External endpoint of the hello-world LoadBalancer service"
-  value       = "http://${kubernetes_service.hello_world_service.status[0].load_balancer[0].ingress[0].hostname}"
-}
+# output "hello_world_service_endpoint" {
+#   description = "External endpoint of the hello-world LoadBalancer service"
+#   value       = "http://${kubernetes_service.hello_world_service.status[0].load_balancer[0].ingress[0].hostname}"
+# }
 
 # output "ecr_image" {
 #   description = "Docker image to be used in the deployment"
