0823094392d595bfd007b9df51cf64a3a91ef0e3
Adam Rocha <adamrocha13@gmail.com>
2025-08-01 16:49:42 -0700

    Uncomment Kubernetes resources for Hello World deployment and service

--- terraform/deploys.tf
+++ terraform/deploys.tf
@@ -1,171 +1,171 @@
-# resource "kubernetes_namespace" "hello_world_ns" {
-#   depends_on = [aws_eks_node_group.node_group]
+resource "kubernetes_namespace" "hello_world_ns" {
+  depends_on = [aws_eks_node_group.node_group]
 
-#   metadata {
-#     name = var.hello_world_ns
-#     labels = {
-#       name = var.hello_world_ns
-#     }
-#   }
+  metadata {
+    name = var.hello_world_ns
+    labels = {
+      name = var.hello_world_ns
+    }
+  }
 
-#   lifecycle {
-#     prevent_destroy = false
-#     # create_before_destroy = true # ensures Terraform can re-create a resource before deletion
-#   }
-# }
+  lifecycle {
+    prevent_destroy = false
+    # create_before_destroy = true # ensures Terraform can re-create a resource before deletion
+  }
+}
 
-# resource "kubernetes_service" "hello_world_service" {
-#   depends_on = [aws_eks_node_group.node_group]
+resource "kubernetes_service" "hello_world_service" {
+  depends_on = [aws_eks_node_group.node_group]
 
-#   metadata {
-#     name      = var.service
-#     namespace = var.hello_world_ns
-#     annotations = {
-#       "service.beta.kubernetes.io/aws-load-balancer-type"        = "nlb"
-#       "service.beta.kubernetes.io/aws-load-balancer-scheme"      = "internet-facing"
-#       "service.beta.kubernetes.io/aws-load-balancer-target-type" = "ip"
-#     }
-#   }
+  metadata {
+    name      = var.service
+    namespace = var.hello_world_ns
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type"        = "nlb"
+      "service.beta.kubernetes.io/aws-load-balancer-scheme"      = "internet-facing"
+      "service.beta.kubernetes.io/aws-load-balancer-target-type" = "ip"
+    }
+  }
 
-#   spec {
-#     selector = {
-#       app = var.deployment
-#     }
+  spec {
+    selector = {
+      app = var.deployment
+    }
 
-#     type = "LoadBalancer"
+    type = "LoadBalancer"
 
-#     port {
-#       name        = "http"
-#       protocol    = "TCP"
-#       port        = 80
-#       target_port = 8080
-#     }
-#   }
-# }
+    port {
+      name        = "http"
+      protocol    = "TCP"
+      port        = 80
+      target_port = 8080
+    }
+  }
+}
 
-# resource "kubernetes_deployment" "hello_world" {
-#   depends_on = [kubernetes_namespace.hello_world_ns]
+resource "kubernetes_deployment" "hello_world" {
+  depends_on = [kubernetes_namespace.hello_world_ns]
 
-#   metadata {
-#     name      = var.deployment
-#     namespace = var.hello_world_ns
-#     labels = {
-#       app = var.deployment
-#     }
-#     annotations = {
-#       "deployment.kubernetes.io/revision" = "1"
-#       "description"                       = "Hello World Deployment"
-#     }
-#   }
+  metadata {
+    name      = var.deployment
+    namespace = var.hello_world_ns
+    labels = {
+      app = var.deployment
+    }
+    annotations = {
+      "deployment.kubernetes.io/revision" = "1"
+      "description"                       = "Hello World Deployment"
+    }
+  }
 
-#   spec {
-#     replicas                  = 3
-#     revision_history_limit    = 10
-#     min_ready_seconds         = 5
-#     progress_deadline_seconds = 300
+  spec {
+    replicas                  = 3
+    revision_history_limit    = 10
+    min_ready_seconds         = 5
+    progress_deadline_seconds = 300
 
-#     strategy {
-#       type = "RollingUpdate"
+    strategy {
+      type = "RollingUpdate"
 
-#       rolling_update {
-#         max_surge       = "25%"
-#         max_unavailable = "25%"
-#       }
-#     }
+      rolling_update {
+        max_surge       = "25%"
+        max_unavailable = "25%"
+      }
+    }
 
-#     selector {
-#       match_labels = {
-#         app = var.deployment
-#       }
-#     }
+    selector {
+      match_labels = {
+        app = var.deployment
+      }
+    }
 
-#     template {
-#       metadata {
-#         labels = {
-#           app = var.deployment
-#         }
-#       }
+    template {
+      metadata {
+        labels = {
+          app = var.deployment
+        }
+      }
 
-#       spec {
-#         automount_service_account_token = false
+      spec {
+        automount_service_account_token = false
 
-#         security_context {
-#           run_as_non_root = true
-#           run_as_user     = 10001
-#         }
+        security_context {
+          run_as_non_root = true
+          run_as_user     = 10001
+        }
 
-#         volume {
-#           name = "nginx-cache"
-#           empty_dir {}
-#         }
+        volume {
+          name = "nginx-cache"
+          empty_dir {}
+        }
 
-#         volume {
-#           name = "nginx-run"
-#           empty_dir {}
-#         }
+        volume {
+          name = "nginx-run"
+          empty_dir {}
+        }
 
-#         container {
-#           name              = var.deployment
-#           image_pull_policy = "Always"
-#           image             = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.repo_name}:${var.image_tag}@${var.image_digest}"
-#           # image             = "adamrocha/hello-world-demo:1.2.0"
-#           # image             = "hashicorp/http-echo:1.0"
-#           # args              = ["-text=👋 Hello from Kubernetes!"]
-#           port {
-#             container_port = 80
-#             protocol       = "TCP"
-#           }
+        container {
+          name              = var.deployment
+          image_pull_policy = "Always"
+          image             = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.repo_name}:${var.image_tag}@${var.image_digest}"
+          # image             = "adamrocha/hello-world-demo:1.2.0"
+          # image             = "hashicorp/http-echo:1.0"
+          # args              = ["-text=👋 Hello from Kubernetes!"]
+          port {
+            container_port = 80
+            protocol       = "TCP"
+          }
 
-#           volume_mount {
-#             name       = "nginx-cache"
-#             mount_path = "/var/cache/nginx"
-#           }
+          volume_mount {
+            name       = "nginx-cache"
+            mount_path = "/var/cache/nginx"
+          }
 
-#           volume_mount {
-#             name       = "nginx-run"
-#             mount_path = "/var/run"
-#           }
+          volume_mount {
+            name       = "nginx-run"
+            mount_path = "/var/run"
+          }
 
-#           resources {
-#             limits = {
-#               cpu    = "100m"
-#               memory = "64Mi"
-#             }
-#             requests = {
-#               cpu    = "50m"
-#               memory = "32Mi"
-#             }
-#           }
-#           security_context {
-#             run_as_user                = 10001
-#             run_as_non_root            = true
-#             allow_privilege_escalation = false
-#             read_only_root_filesystem  = true
-#             seccomp_profile {
-#               type = "RuntimeDefault"
-#             }
-#             capabilities {
-#               drop = ["ALL"]
-#             }
-#           }
-#           liveness_probe {
-#             http_get {
-#               path = "/"
-#               port = 8080
-#             }
-#             initial_delay_seconds = 10
-#             period_seconds        = 10
-#           }
-#           readiness_probe {
-#             http_get {
-#               path = "/"
-#               port = 8080
-#             }
-#             initial_delay_seconds = 5
-#             period_seconds        = 5
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+          resources {
+            limits = {
+              cpu    = "100m"
+              memory = "64Mi"
+            }
+            requests = {
+              cpu    = "50m"
+              memory = "32Mi"
+            }
+          }
+          security_context {
+            run_as_user                = 10001
+            run_as_non_root            = true
+            allow_privilege_escalation = false
+            read_only_root_filesystem  = true
+            seccomp_profile {
+              type = "RuntimeDefault"
+            }
+            capabilities {
+              drop = ["ALL"]
+            }
+          }
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 8080
+            }
+            initial_delay_seconds = 10
+            period_seconds        = 10
+          }
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = 8080
+            }
+            initial_delay_seconds = 5
+            period_seconds        = 5
+          }
+        }
+      }
+    }
+  }
+}
