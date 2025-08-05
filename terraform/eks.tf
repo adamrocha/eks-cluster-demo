dfb93b6d4b2c554194cb8720ff5591bc96315af6
Adam Rocha <adamrocha13@gmail.com>
2025-08-04 17:19:22 -0700

    Update desired size of EKS node group from 4 to 3

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -47,7 +47,7 @@ resource "aws_eks_node_group" "node_group" {
   subnet_ids      = aws_subnet.public[*].id
 
   scaling_config {
-    desired_size = 4
+    desired_size = 3
     min_size     = 2
     max_size     = 6
   }
