28f69d414e72c78fd26423156d8da61c11dc1ef5
Adam Rocha <adamrocha13@gmail.com>
2025-08-09 05:58:44 -0700

    Update Prometheus Helm release configuration and add port-forwarding for Grafana and Node Exporter

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -38,44 +38,88 @@ resource "helm_release" "prometheus" {
   wait             = false
 
   set {
-    name  = "prometheus.service.type"
-    value = "LoadBalancer"
+    name  = "prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues"
+    value = false
   }
 
+  # set {
+  #   name  = "prometheus.prometheusSpec.serviceMonitorSelector"
+  #   value = "{}"
+  # }
+
   set {
-    name  = "prometheus.service.loadBalancerType"
-    value = "nlb"
+    name  = "grafana.enabled"
+    value = true
   }
 
   set {
     name  = "grafana.service.type"
-    value = "LoadBalancer"
+    value = "ClusterIP"
   }
 
   set {
-    name  = "grafana.service.loadBalancerType"
-    value = "nlb"
+    name  = "grafana.service.port"
+    value = "80"
   }
-  set {
-    name  = "fullnameOverride"
-    value = "prometheus"
-  }
-
-  set {
-    name  = "global.serviceAnnotations.service.beta.kubernetes.io/aws-load-balancer-connection-draining-enabled"
-    value = "false"
-  }
-
-  # Prometheus Service Finalizer Off
-  set {
-    name  = "prometheus.service.annotations.service\\.kubernetes\\.io/load-balancer-cleanup"
-    value = "\"true\""
-  }
-
-  # Grafana Service Finalizer Off
-  set {
-    name  = "grafana.service.annotations.service\\.kubernetes\\.io/load-balancer-cleanup"
-    value = "\"true\""
-  }
-
 }
+
+resource "null_resource" "prometheus_port_forward" {
+  depends_on = [helm_release.prometheus]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "Starting Prometheus port-forward..."
+      kubectl port-forward svc/prometheus-grafana 3000:80 -n monitoring-ns >/tmp/vault-pf.log 2>&1 &
+      kubectl port-forward svc/prometheus-node-exporter 9100:9100 -n monitoring-ns >/tmp/vault-pf.log 2>&1 &
+      echo "Grafana UI should be available at http://localhost:3000/ui"
+      echo "Node Exporter UI should be available at http://localhost:9100/"
+      echo "To stop port-forward, kill the background process:"
+      echo "  pkill -f 'kubectl port-forward svc/prometheus-grafana -n monitoring-ns 3000:80'"
+      echo "  pkill -f 'kubectl port-forward svc/prometheus-node-exporter -n monitoring-ns 9100:9100'"
+    EOT
+    # Keep this running during apply, or run detached (this is a simple fire-and-forget)
+  }
+}
+
+#   set {
+#     name  = "prometheus.service.type"
+#     value = "LoadBalancer"
+#   }
+
+#   set {
+#     name  = "prometheus.service.loadBalancerType"
+#     value = "nlb"
+#   }
+
+#   set {
+#     name  = "grafana.service.type"
+#     value = "LoadBalancer"
+#   }
+
+#   set {
+#     name  = "grafana.service.loadBalancerType"
+#     value = "nlb"
+#   }
+#   set {
+#     name  = "fullnameOverride"
+#     value = "prometheus"
+#   }
+
+#   set {
+#     name  = "global.serviceAnnotations.service.beta.kubernetes.io/aws-load-balancer-connection-draining-enabled"
+#     value = "false"
+#   }
+
+#   # Prometheus Service Finalizer Off
+#   set {
+#     name  = "prometheus.service.annotations.service\\.kubernetes\\.io/load-balancer-cleanup"
+#     value = "\"true\""
+#   }
+
+#   # Grafana Service Finalizer Off
+#   set {
+#     name  = "grafana.service.annotations.service\\.kubernetes\\.io/load-balancer-cleanup"
+#     value = "\"true\""
+#   }
+
+# }
