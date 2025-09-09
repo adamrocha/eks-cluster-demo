2d8964f4977244e653abcc8f5ea385fb2fb4fecb
Adam Rocha <adamrocha13@gmail.com>
2025-09-09 02:44:49 -0700

    Refactor backend configuration in Terraform to comment out dynamodb_table setting

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-2727"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    use_lockfile   = true
-    encrypt        = true
+    bucket = "terraform-state-bucket-2727"
+    key    = "envs/dev/terraform.tfstate"
+    region = "us-east-1"
+    # dynamodb_table = "terraform-locks"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
