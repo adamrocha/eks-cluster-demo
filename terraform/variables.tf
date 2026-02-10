24746fda3aff5d4df9f716e98cc6b75cfeafb6e2
Adam Rocha <adamrocha13@gmail.com>
2026-02-09 20:47:47 -0800

    fix: update kustomization.yaml with config map generator example and refactor platform variable in locals.tf and variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -46,21 +46,6 @@ variable "deployment" {
   type        = string
 }
 
-variable "instance_type" {
-  description = "EC2 instance type for the EKS node group"
-  default     = "t3.small"
-  # default     = "t4g.small"
-  type = string
-}
-
-variable "ami_type" {
-  description = "EC2 AMI type for the EKS node group"
-  default     = "AL2023_x86_64_STANDARD"
-  # default     = "AL2023_ARM_64_STANDARD"
-  type = string
-}
-
-
 variable "repo_name" {
   description = "ECR repository name"
   default     = "hello-world-demo"
@@ -83,4 +68,25 @@ variable "platforms" {
   description = "Platforms for Docker buildx"
   default     = ["linux/amd64", "linux/arm64"]
   type        = list(string)
+}
+
+variable "platform" {
+  description = "Platform for Docker build"
+  default     = "linux/amd64"
+  # default     = "linux/arm64"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for the EKS node group"
+  default     = "t3.small"
+  # default     = "t4g.small"
+  type = string
+}
+
+variable "ami_type" {
+  description = "EC2 AMI type for the EKS node group"
+  default     = "AL2023_x86_64_STANDARD"
+  # default     = "AL2023_ARM_64_STANDARD"
+  type = string
 }
